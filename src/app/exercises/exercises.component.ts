9e36c86d1669949b7d8fe657c8f9e1468a937c7f
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-05-21 13:24:33 +0200

    create new component exercises and show it in app.component

---
+++ src/app/exercises/exercises.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercises',
+  templateUrl: './exercises.component.html',
+  styleUrls: ['./exercises.component.css']
+})
+export class ExercisesComponent implements OnInit {
+
+  exercise = 'Push-up'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
