aa59d6cecf045bddfc2e7abb580fe120fb5f8c85
Enrique Sanchez <enrique.sanchez@docomodigital.com>
2021-05-25 10:25:36 +0200

    show only 4 exercises. add panel link

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -9,14 +9,20 @@ import { ExerciseService } from '../exercise.service';
 })
 export class PanelComponent implements OnInit {
 
-  exercises!:Exercise[];
+  exercises:Exercise[];
 
-  constructor(private exerciseService:ExerciseService) { }
+  constructor(private exerciseService:ExerciseService) {
+    console.log("Panel Constructor");
+    this.exercises = [];
+  }
 
   ngOnInit(): void {
+    console.log("Initialize Panel Component");
+    this.getExercises();
   }
 
   getExercises():void {
-    this.exerciseService.getExercises().subscribe(exercises=>this.exercises=exercises);
+    this.exerciseService.getExercises().subscribe(exercises=>this.exercises=exercises.slice(1, 5));
+    console.log('TEST-' + JSON.stringify(this.exercises));
   }
 }
